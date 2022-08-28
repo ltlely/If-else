48a967f9d7da0a045824381621fb0146f18b2f71
ltlely <96932263+ltlely@users.noreply.github.com>
2022-08-27 22:13:58 -0400

    Add files via upload

---
+++ Main(10).java
@@ -0,0 +1,28 @@
+/*  Print the two strings, firstString and secondString, in alphabetical order. Assume the strings are lowercase.
+End with newline. Sample output:
+
+capes rabbits */
+
+
+import java.util.Scanner;
+
+public class OrderStrings {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      String firstString;
+      String secondString;
+
+      firstString  = scnr.next();
+      secondString = scnr.next();
+
+      /* Your solution goes here  */
+      if (secondString.compareTo(firstString) < 0) {
+      System.out.println (secondString + " " + firstString );
+      }
+      else {
+      System.out.println (firstString + " " + secondString );
+      }
+      
+
+   }
+}
